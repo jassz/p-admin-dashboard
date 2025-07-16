34462cf02bd1e9d8d54597cfc31179e71b1a9160
devnaja <201055297+najanadhirahh@users.noreply.github.com>
2025-07-16 09:11:17 +0800

    fix: change coming soon page into global component

--- src/pages/Homepage/index2.jsx
+++ src/pages/Homepage/index2.jsx
@@ -1,47 +1,12 @@
 import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import PrivateLayout from "../../layouts/privateLayout";
-import myImage from './../../assets/images/coming-soon.jpg'; // adjust the path
-
+import ComingSoon from "components/comingSoon";
 
 export default function Homepage() {
-  const [selectedNode, setSelectedNode] = useState(null);
-
-  const handleSectionAClick = (event, itemId) => {
-    console.log(`Nodes ${itemId} is clicked`);
-  };
-
-  const handleItemSelectionToggle = (event, itemId, isSelected) => {
-    if (isSelected) {
-      setSelectedNode(itemId);
-    }
-  };
-
   return (
     <PrivateLayout>
-  <Box
-    display="flex"
-    flexDirection="column"
-    alignItems="end"
-    justifyContent="center"
-    height="90vh"
-    gap={2}
-     sx={{
-    // backgroundColor: 'tertiary.main', 
-    backgroundImage: `url(${myImage})`,
-    backgroundSize: 'cover',        // Optional: covers entire box
-    backgroundRepeat: 'no-repeat',  // Optional: prevents tiling
-    backgroundPosition: 'center',   // Optional: centers image
-  }}
-  >
-    <Typography variant="h4" textTransform={"uppercase"} fontWeight={'bold'} px={5}  >
-      Welcome to Poisum Admin Dashboard!
-    </Typography>
-    {/* <Typography variant="body1" fontWeight='light'  >
-      Time to spark some culture, captain!
-      Poisum’s ready — are you? 
-    </Typography> */}
-  </Box>
-</PrivateLayout>
+      <ComingSoon title="Poisum Admin Dashboard!" />
+    </PrivateLayout>
   );
 }
